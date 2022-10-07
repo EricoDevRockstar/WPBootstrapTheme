18aa63699ac724f144446d8dd115646497a33256
Eric W. M <ericmwangi0916@gmail.com>
2022-10-08 02:22:41 +0300

    Made Template part for Index file

--- index.php
+++ index.php
@@ -23,39 +23,6 @@ get_template_part('template-parts/banner', 'hero');
             <a href="#" class="read-more">Read the full post -&gt;</a>
           </article>
 
-          <article class="post my-5">
-            <div class="meta">
-              <span>Sep 24, 2021</span>
-            </div>
-            <h2><a href="#">Blog post title</a></h2>
-            <p class="excerpt">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
-            </p>
-            <a href="#" class="read-more">Read the full post -&gt;</a>
-          </article>
-
-          <article class="post my-5">
-            <div class="meta">
-              <span>Sep 24, 2021</span>
-            </div>
-            <h2><a href="#">Blog post title</a></h2>
-            <p class="excerpt">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
-            </p>
-            <a href="#" class="read-more">Read the full post -&gt;</a>
-          </article>
-
-          <article class="post my-5">
-            <div class="meta">
-              <span>Sep 24, 2021</span>
-            </div>
-            <h2><a href="#">Blog post title</a></h2>
-            <p class="excerpt">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
-            </p>
-            <a href="#" class="read-more">Read the full post -&gt;</a>
-          </article>
-
         </div>
       </div>
     </div>

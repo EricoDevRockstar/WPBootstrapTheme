7312d10e76826fe9caa1b756d1508853fecdd3b8
Eric W. M <ericmwangi0916@gmail.com>
2022-10-08 18:52:35 +0300

    Updated Content None file

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -7,12 +7,41 @@
  * 
  */
 
- if ( is_home() && current_user_can( 'publish_posts' ) ) {
+if ( is_home() && current_user_can( 'publish_posts' ) ) {
 
-
- } elseif( is_search() ) {
-
- } else {
-
+    printf(
+          '<p>' . wp_kses(
+              /* translators: 1: link to WP admin new post page. */
+              __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'WPBootstrapTheme' ),
+              array(
+                  'a' => array(
+                      'href' => array(),
+                  ),
+              )
+          ) . '</p>',
+          esc_url( admin_url( 'post-new.php' ) )
+      );
+ 
+  } elseif ( is_search() ) {
+ ?>
+ 
+ <div class="search-results-none">
+   <h2><?php esc_html_e( 'Not Found', 'WPBootstrapTheme' ); ?></h2>
+   <p><?php esc_html_e( 'The content you are looking for is no longer available.', 'WPBootstrapTheme' ); ?></p>
+ </div>
+ 
+ <?php
+   get_search_form();
+ 
+  } else {
+ ?>
+ 
+ <div class="search-results-none">
+   <h2><?php esc_html_e( 'Not Found', 'WPBootstrapTheme' ); ?></h2>
+   <p><?php esc_html_e( 'The content you are looking for is no longer available.', 'WPBootstrapTheme' ); ?></p>
+ </div>
+ 
+ <?php
+   get_search_form();
  }
-?>
+ ?>

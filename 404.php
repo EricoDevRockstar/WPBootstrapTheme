dc278ddea2111d0aad9ca3cada735a3f9cbc0150
Eric W. M <ericmwangi0916@gmail.com>
2022-10-15 02:04:08 +0300

    Update the 4 files

--- 404.php
+++ 404.php
@@ -15,45 +15,23 @@
 ?>
 
 <div class="content-area">
-    <div class="container">
-        <div class="row">
-        <?php
-            // Are there any posts in the DB?
-            if ( have_posts() ) {
-            while( have_posts() ) {
-                the_post();
-                if ( has_post_thumbnail() ) {
-                    the_post_thumbnail(
-                        'large',
-                        array(
-                            'class' => 'image-fluid'
-                        )
-                );
-                }
-        ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2 col-sm-12 offset-sm-0 overflow-hidden">
 
-        <div class="col-md-8 offset-md-2 overflow-hidden">
-
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-            <?php
-                // Output the post content
-                
-                    the_content();
-            ?>
-
-        </article>
+        <div class="image-404">
+          <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/404.png" alt="404" class="img-fluid">
         </div>
 
-        <?php
-            // Output this incase there are no posts
-            }
-            } else {
-                get_template_part( 'template-parts/content', 'none');
-            }
-        ?>
+        <h3 class="text-center"><?php esc_html_e( 'Sorry, sad pug can\'t find it!', 'bootstrap2wordpress' ); ?></h3>
+
+        <div class="button-box text-center">
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="btn btn-primary"><?php esc_html_e( 'Back to Homepage', 'bootstrap2wordpress' ); ?></a>
         </div>
+
+      </div>
     </div>
+  </div>
 </div>
 
 <?php get_footer();
